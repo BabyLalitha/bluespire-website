5fbda9cc4f04082ed9b906b249346f446b230d5e
goelbhrat02 <goelbhrat02.bg@gmail.com>
2023-10-27 14:47:43 +0530

    Landing and home page added

---
+++ codeBase/app/HomeFourth.js
@@ -0,0 +1,65 @@
+import React from 'react'
+
+function HomeFourth() {
+    return (
+        <div>
+            <div>
+                {/* text container */}
+                <div className='text-center w-[100%]'>
+                    <h2 className='text-4xl font-medium'>
+                        You’re in good company
+                    </h2>
+                    <div className='w-[70vw] mx-auto'>
+                        <p className='text-2xl font-light my-7'>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                        </p>
+                    </div>
+                </div>
+
+                {/* box container */}
+                <div className='my-16'>
+                    <div className='w-[80vw] mx-auto grid md:grid-cols-4 grid-cols-2 gap-5 gap-y-20'>
+                        <div className='h-[60px] w-[150px] bg-gray-200 mx-auto'>
+                        </div>
+                        <div className='h-[60px] w-[150px] bg-gray-200 mx-auto'>
+                        </div>
+                        <div className='h-[60px] w-[150px] bg-gray-200 mx-auto'>
+                        </div>
+                        <div className='h-[60px] w-[150px] bg-gray-200 mx-auto'>
+                        </div>
+                        <div className='h-[60px] w-[150px] bg-gray-200 mx-auto'>
+                        </div>
+                        <div className='h-[60px] w-[150px] bg-gray-200 mx-auto'>
+                        </div>
+                        <div className='h-[60px] w-[150px] bg-gray-200 mx-auto'>
+                        </div>
+                        <div className='h-[60px] w-[150px] bg-gray-200 mx-auto'>
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <div className='w-[100%] md:h-[30vh] h-auto bg-light-gray flex flex-col md:flex-row justify-center items-center'>
+                        <div className='md:w-[50vw] w-[90%] pt-7 pb-5'>
+                            <p className='text-center text-3xl font-extralight '>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                            </p>
+                        </div>
+                        <div className='md:w-[40vw] pb-7 md:pb-0 w-[100%] flex justify-center items-center'>
+                            <div className='md:ml-10 flex justify-center items-center w-auto'>
+                                <button className='w-32 border-2 rounded-3xl border-blue text-blue hover:bg-blue hover:text-white mr-10'>
+                                    Join Today
+                                </button>
+                                <button className='w-32 border-2 rounded-3xl border-blue text-blue hover:bg-blue hover:text-white'>
+                                    Contact Us
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HomeFourth
---
+++ codeBase/app/HomeService.js
@@ -0,0 +1,83 @@
+import React from 'react'
+
+function HomeService() {
+    return (
+        <div className='font-poppins'>
+            <div className='h-auto w-[100%] flex flex-col items-center'>
+
+                {/* *************Heading************** */}
+                <div className='w-[90vw] my-16'>
+                    <p className='font-poppins text-4xl text-center font-normal'>
+                        Within the vast blue canvas of inspiration, we weave logic, creativity, and curiosity to craft, solve, and create.
+                    </p>
+                </div>
+
+                {/* ***************Services container**************** */}
+                <div className='grid grid-cols-1 sm:grid-cols-2  gap-2 gap-y-16 w-[100%]'>
+                    <div className='w-[90vw] sm:w-[40vw] mx-auto'>
+                        <div className='mb-4'>
+                            <p className='text-lg font-semibold'>
+                                Consult And Design
+                            </p>
+                        </div>
+                        <div className='mb-6'>
+                            <p className='text-md'>
+                                Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est.
+                            </p>
+                        </div>
+                        <button className='border-2 border-blue rounded-3xl px-5 text-blue hover:bg-blue hover:text-white'>
+                            Learn More
+                        </button>
+                    </div>
+                    <div className='w-[90vw] sm:w-[40vw] mx-auto'>
+                        <div className='mb-4'>
+                            <p className='text-lg font-semibold'>
+                                Engineer & Automate
+                            </p>
+                        </div>
+                        <div className='mb-6'>
+                            <p className='text-md'>
+                                Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est.
+                            </p>
+                        </div>
+                        <button className='border-2 border-blue rounded-3xl px-5 text-blue hover:bg-blue hover:text-white'>
+                            Learn More
+                        </button>
+                    </div>
+                    <div className='w-[90vw] sm:w-[40vw] mx-auto'>
+                        <div className='mb-4'>
+                            <p className='text-lg font-semibold'>
+                                Analyze & Optimize
+                            </p>
+                        </div>
+                        <div className='mb-6'>
+                            <p className='text-md'>
+                                Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est.
+                            </p>
+                        </div>
+                        <button className='border-2 border-blue rounded-3xl px-5 text-blue hover:bg-blue hover:text-white'>
+                            Learn More
+                        </button>
+                    </div>
+                    <div className='w-[90vw] sm:w-[40vw] mx-auto'>
+                        <div className='mb-4'>
+                            <p className='text-lg font-semibold'>
+                                Cloud Solutions
+                            </p>
+                        </div>
+                        <div className='mb-6'>
+                            <p className='text-md'>
+                                Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est.
+                            </p>
+                        </div>
+                        <button className='border-2 border-blue rounded-3xl px-5 text-blue hover:bg-blue hover:text-white'>
+                            Learn More
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HomeService
