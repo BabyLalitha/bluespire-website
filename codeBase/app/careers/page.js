1a1c6e6d8bc8711ed6b916175dcc396c25fc006d
Nisha Manikyala <nishamanikyala@gmail.com>
2023-11-02 16:01:54 +0530

    Careers- JD, ApplyForm

---
+++ codeBase/app/careers/page.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Link from 'next/link';  
+import jobData from './jobsData.json';
+
+const CareersPage = () => {
+  return (
+    <div>
+      <h1>Welcome to Our Careers Page</h1>
+
+      <div className="job-listings">
+        {jobData.map(job => (
+          <div key={job.id}>
+            {/* <h2>{job.title}</h2> */}
+            {/* <p>{job.description}</p> */}
+            <Link href={`/careers/jd?id=${job.id}`} passHref>
+              {job.title}
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default CareersPage;
